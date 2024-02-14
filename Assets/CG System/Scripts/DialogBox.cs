d42dda61836c6adbdea3fa929efe4c7aef18a584
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2024-02-14 17:56:45 +0800

    feat: 实现对话框的播放功能

--- Assets/CG System/Scripts/DialogBox.cs
+++ Assets/CG System/Scripts/DialogBox.cs
@@ -6,27 +6,41 @@ using UnityEngine.UI;
 
 namespace CG
 {
-    /// <summary>
-    /// 旁白类，控制旁白的显示和隐藏
-    /// </summary>
-    public class Narration : TextBlock
+    public class DialogBox : TextBlock
     {
         [SerializeField] private float _duration = 1f; // 渐变时长
         [SerializeField] private float _fastForwardDuration = 0.1f; // 快进时渐变时长
 
-        private Image _image;   // 旁白框
+        private Image _dialogBox;   // 对话框
+        private Image _expression;  // 表情
+        private Image _nameStamp;   // 名章
+        private Image[] _imagesToChange;   // 需要改变的图片
         private Color _color;   // 没有隐藏时的颜色
         private bool _isHiding = false; // 是否隐藏中
         private float _timer = 0f;  // 计时器
 
-        /// <summary>
-        /// 开始或继续播放旁白
-        /// </summary>
-        /// <param name="cancellationToken">取消令牌</param>
-        /// <param name="fastForward">是否快进播放</param>
-        public override async UniTask PlayBlock(CancellationToken cancellationToken, bool fastForward = true)
+        public void SetImagesToChange(bool needChangeDialogBox)
         {
-            gameObject.SetActive(true);
+            if (needChangeDialogBox)
+            {
+                _imagesToChange = new Image[] { _dialogBox, _expression, _nameStamp };
+            }
+            else
+            {
+                _imagesToChange = new Image[] { _expression, _nameStamp };
+            }
+        }
+
+        public void SetDialog(Image dialogBox, Image expression, Image nameStamp, string text)
+        {
+            _dialogBox = dialogBox;
+            _expression = expression;
+            _nameStamp = nameStamp;
+            _textMeshPro.text = text;
+        }
+
+        public override async UniTask PlayBlock(CancellationToken cancellationToken, bool fastForward = false)
+        {
             float duration = fastForward ? _fastForwardDuration : _duration;
 
             while (true)
@@ -37,7 +51,8 @@ namespace CG
                 }
                 if (_timer > duration)
                 {
-                    _image.color = _isHiding ? Color.clear : _color;
+                    Array.ForEach(_imagesToChange, image => image.color = _isHiding ? Color.clear : _color);
+                    _textMeshPro.color = _textColor;
                     await StartTyping(cancellationToken, fastForward);
                     if (cancellationToken.IsCancellationRequested)
                     {
@@ -47,17 +62,14 @@ namespace CG
                     break;
                 }
                 _color.a = Mathf.Lerp(0f, 1f, _timer / duration);
-                _image.color = _isHiding ? Color.clear : _color;
+                Array.ForEach(_imagesToChange, image => image.color = _isHiding ? Color.clear : _color);
+                // ? Why is next line not working?
+                // _imagesToChange.ForEach(image => image.color = _isHiding ? Color.clear : _color);
                 _timer += Time.deltaTime;
                 await UniTask.Yield();
             }
         }
 
-        /// <summary>
-        /// 退出旁白
-        /// </summary>
-        /// <param name="cancellationToken">取消令牌</param>
-        /// <param name="fastForward">是否快进播放</param>
         public override async UniTask ExitBlock(CancellationToken cancellationToken, bool fastForward = false)
         {
             float duration = fastForward ? _fastForwardDuration : _duration;
@@ -70,16 +82,15 @@ namespace CG
                 }
                 if (_timer > duration)
                 {
-                    // TODO: 通知外部旁白播放结束
-                    _image.color = Color.clear;
+                    // TODO: 通知外部对话播放结束
+                    Array.ForEach(_imagesToChange, image => image.color = Color.clear);
                     _textMeshPro.color = Color.clear;
-                    gameObject.SetActive(false);
                     _timer = 0f;
                     break;
                 }
                 _color.a = Mathf.Lerp(1f, 0f, _timer / duration);
-                _image.color = _isHiding ? Color.clear : _color;
-                Color color = _textColor;
+                Array.ForEach(_imagesToChange, image => image.color = _color);
+                Color color = _textMeshPro.color;
                 color.a = _color.a;
                 _textMeshPro.color = color;
                 _timer += Time.deltaTime;
@@ -87,39 +98,39 @@ namespace CG
             }
         }
 
-        /// <summary>
-        /// 隐藏旁白框及文字，展示背景
-        /// </summary>
         public override void HideBlock()
         {
             _isHiding = true;
-            _image.color = Color.clear;
+            _dialogBox.color = Color.clear;
+            _expression.color = Color.clear;
+            _nameStamp.color = Color.clear;
             _textMeshPro.color = Color.clear;
         }
 
-        /// <summary>
-        /// 显示旁白框及文字
-        /// </summary>
         public override void ShowBlock()
         {
             _isHiding = false;
-            _image.color = _color;
+            _dialogBox.color = _color;
+            _expression.color = _color;
+            _nameStamp.color = _color;
             _textMeshPro.color = _textColor;
         }
 
         private new void Awake()
         {
             base.Awake();
-            _image = GetComponent<Image>();
+            _dialogBox = GameObject.Find("DialogBox").GetComponent<Image>();
+            _expression = GameObject.Find("Expression").GetComponent<Image>();
+            _nameStamp = GameObject.Find("NameStamp").GetComponent<Image>();
         }
 
         private new void Start()
         {
             base.Start();
-            gameObject.SetActive(false);
             _color = Color.white;
             _color.a = 0f;
-            _image.color = Color.clear;
+            Array.ForEach(_imagesToChange, image => image.color = Color.clear);
+            _imagesToChange = new Image[] { _dialogBox, _expression, _nameStamp };
         }
     }
 }
