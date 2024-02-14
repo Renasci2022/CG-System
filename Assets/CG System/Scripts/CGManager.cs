0d9f29dfcf7177bc6f49abb9cb76d2473c80fdc7
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2024-02-14 12:05:08 +0800

    feat: 初步实现 CGManager 和异步方法取消恢复的功能
    通过维护 _playMethods 列表，管理异步方法的取消和恢复

---
+++ Assets/CG System/Scripts/CGManager.cs
@@ -0,0 +1,96 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace CG
+{
+    /// <summary>
+    /// CG 管理器
+    /// 调用 CGPlayer 的方法，维护状态机
+    /// </summary>
+    public class CGManager : MonoBehaviour
+    {
+        private CGState _state = CGState.None; // 当前状态
+        private CGState _lastState = CGState.None; // 上一个状态
+
+        private delegate UniTask PlayMethod(CancellationToken cancellationToken);   // 播放方法
+        [ShowInInspector] private PlayMethod[] _playMethods;   // 播放方法数组
+
+        private CancellationTokenSource _cancellationTokenSource;    // 取消令牌
+
+        private CGPlayer _player; // CG 播放器
+
+        [Button]
+        public async UniTask Play()
+        {
+            _lastState = _state;
+            _state = CGState.Playing;
+            _cancellationTokenSource = new CancellationTokenSource();
+            await UniTask.WhenAll(_playMethods.Select(method => method(_cancellationTokenSource.Token)));
+
+            if (_cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                return;
+            }
+            // TODO: 播放完毕后的处理，发送信号等
+            _playMethods = new PlayMethod[] { };
+        }
+
+        [Button]
+        public void Pause()
+        {
+            _lastState = _state;
+            _state = CGState.Paused;
+            _cancellationTokenSource.Cancel();
+        }
+
+        [Button]
+        public async UniTask Resume()
+        {
+            if (_lastState == CGState.Playing)
+            {
+                await Play();
+            }
+            else if (_lastState == CGState.Waiting)
+            {
+                await UniTask.DelayFrame(1);
+                _state = CGState.Waiting;
+            }
+        }
+
+        [Button]
+        public void Test_PlayScene()
+        {
+            _playMethods = new PlayMethod[] { _player.PlayScene };
+        }
+
+        [Button]
+        public void Test_NextTextBlock()
+        {
+            _playMethods = new PlayMethod[] { _player.NextTextBlock };
+        }
+
+        [Button]
+        public void Test_ClearMethods()
+        {
+            _playMethods = new PlayMethod[] { };
+        }
+
+        private void Awake()
+        {
+            _player = GameObject.Find("CGPlayer").GetComponent<CGPlayer>();
+        }
+    }
+
+    public enum CGState
+    {
+        None,
+        Playing,
+        Waiting,
+        Paused,
+        FastForwarding,
+        Hiding,
+    }
+}
