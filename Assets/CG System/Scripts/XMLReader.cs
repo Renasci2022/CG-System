006a7ba44eb3808bb56aebb8f99c95cbee9e0846
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2024-03-05 21:58:35 +0800

    fix: 修复初始化时空引用的问题
    XMLReader 中的特性 Text 修改为实时查找 CGPlayer，但可能导致性能问题

--- Assets/CG System/Scripts/XMLReader.cs
+++ Assets/CG System/Scripts/XMLReader.cs
@@ -83,14 +83,14 @@ namespace CG
         [XmlElement("English")]
         public string English;
 
+        // ? 是否存在性能问题
         [XmlIgnore]
-        public string Text => CGPlayer.Instance.Language == Language.Chinese ? Chinese : English;
+        public string Text => GameObject.FindObjectOfType<CGPlayer>().Language == Language.Chinese ? Chinese : English;
 
         [XmlIgnore]
         public (DialogBoxType, string, string, EffectType) DialogInfo => (DialogBox, Character, Expression, Effect);
     }
 
-    // TODO: 补充更多类型
     public enum LineType
     {
         Scene,
